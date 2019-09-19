ecd386832984acdcb39ff51d6d630d247a8b4839
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2019-09-19 21:33:28 +0300

    indentation

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def main():
 	OLED = OLED_Driver.OLED()
 	OLED_ScanDir = OLED_Driver.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
 
-    image = Image.fromarray(test_1()).convert('L')
+	image = Image.fromarray(test_1()).convert('L')
 
 	OLED.OLED_Init(OLED_ScanDir)
 	OLED.OLED_ShowImage(image, 0, 0)
 
 if __name__ == '__main__':
-    main()
+	main()
