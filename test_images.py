b2443ed407d357dfa74547331ee40cc7c22b9f32
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2019-09-22 23:13:26 +0300

    debug features

--- test_images.py
+++ test_images.py
@@ -1,6 +1,7 @@
 from utils import *
-from math import pi
+from math import pi, sqrt
 from random import randint
+import argparse
 
 tests = []
 def d_(f):
@@ -8,7 +9,7 @@ def d_(f):
     return f
 
 @d_
-def test_1(show=False):
+def test_1():
     paths = []
     w = W/8.0
 
@@ -18,10 +19,10 @@ def test_1(show=False):
     paths.append(circle(w*2, 90, w*1.2, 4))
     paths.append(circle(w*6, 90, w*1.3, 5, -pi/2.0))
 
-    return plot_paths(paths, show=show)
+    return plot_paths(paths)
 
 @d_
-def test_2(show=False):
+def test_2():
     paths = []
 
     paths.append([[0, 0], [127, 0]])
@@ -33,10 +34,10 @@ def test_2(show=False):
     paths.append([[0, 0], [127, 127]])
     paths.append([[0, 127], [127, 0]])
 
-    return plot_paths(paths, show=show)
+    return plot_paths(paths)
 
 @d_
-def test_3(show=False):
+def test_3():
     image = create_image()
 
     for x in range(0, randint(2, 10)):
@@ -46,11 +47,10 @@ def test_3(show=False):
         y1 = randint(0, 127)
         image = plot_paths_on_image([[[x0, y0], [x1, y1]]], image)
 
-    if show: show_image(image)
     return image
 
 @d_
-def test_4(show=False):
+def test_4():
     image = create_image()
 
     for i in range(40):
@@ -58,29 +58,26 @@ def test_4(show=False):
         y = randint(0, 127)
         image.putpixel((x, y), 255)
 
-    if show: show_image(image)
     return image
 
 @d_
-def test_5(show=False):
+def test_5():
     image = create_image()
 
     for y in range(0, 128):
         for x in range((int(400/(y+1))+1)%127, 128, int(400/(y+1))+1):
             image.putpixel((x, y), 255)
 
-    if show: show_image(image)
     return image
 
 #@d_
-def test_6(show=False):
+def test_6():
     image = create_image()
     draw = ImageDraw.Draw(image)
     draw.ellipse((0, 0, 127, 127), fill='white')
     return image
 
-
-def load_img(filename, show=False):
+def load_img(filename):
     return Image.open('src/'+filename).convert('L')
 
 @d_
@@ -101,5 +98,25 @@ def dump():
         l = map(lambda xs: list(map(t, xs)), np.array(tf()).tolist())
         json.dump(list(l), open('jsons/'+tf.__name__+'.json', 'w'))
 
+def show():
+    h = round(sqrt(len(tests)))
+    img = Image.new('L', (h*128, h*128))
+
+    i=0
+    for x in range(h):
+        for y in range(h):
+            i+=1
+            if i<len(tests):
+                img.paste(tests[i](), (x*128, y*128))
+    img.show()
+
 if __name__ == '__main__':
-    dump()
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--dump', action='store_true', default=False)
+    parser.add_argument('--show', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    if args.dump: dump()
+    if args.show: show()
